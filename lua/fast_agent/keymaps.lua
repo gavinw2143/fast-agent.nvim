19c240f6618b5b833c9190b9649f0a8a2eaa6abb
Computer <gavinw2143@gmail.com>
2025-06-19 18:48:01 -0400

    UI changes

--- lua/fast_agent/keymaps.lua
+++ lua/fast_agent/keymaps.lua
@@ -3,15 +3,10 @@ local M = {}
 --- Install default keymaps for FastAgent commands.
 function M.install_default_keymaps()
 	local opts = { silent = true }
-	vim.keymap.set("n", "<leader>gp", "<Cmd>FastAgentPrompt<CR>", opts)
-	vim.keymap.set("n", "<leader>gl", "<Cmd>FastAgentList<CR>", opts)
-	vim.keymap.set("n", "<leader>gs", "<Cmd>FastAgentSwitch<CR>", opts)
-	vim.keymap.set("n", "<leader>gr", "<Cmd>FastAgentFetch<CR>", opts)
-	vim.keymap.set("n", "<leader>ga", "<Cmd>FastAgentAppend<CR>", opts)
 	vim.keymap.set(
 		"n",
 		"<Space>gh",
-		"<Cmd>lua require('fast_agent.ui').toggle_home_panel()<CR>",
+		"<Cmd>lua require('fast_agent.ui.window').toggle_home_menu()<CR>",
 		opts
 	)
 end
